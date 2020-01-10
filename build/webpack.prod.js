3c082cc04383820b0d563ce81c35179c2f306372
wxm <wangxiaoming92@foxmail.com>
2020-01-11 00:12:58 +0800

    引入webpack-bundle-analyzer分析打包后的文件

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -6,42 +6,54 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const WebpackParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
 
-module.exports = WebpackMerge(webpackConfig, {
-    mode: 'production',
-    devtool: 'cheap-module-source-map',
-    optimization: {
-        minimizer: [
-            // new UglifyJsPlugin({//压缩js
-            //     cache: true,
-            //     parallel: true,
-            //     sourceMap: true
-            // }),
-            new WebpackParallelUglifyPlugin({
-                cacheDir: '.cache/',
-                uglifyJs: {
-                    output: {
-                        comments: false,
-                        beatify: false
-                    },
-                    compress: {
-                        drop_console: true,
-                        collapse_Vars: true,
-                        reduce_vars: true
-                    }
-                }
-            }),
-            new OptimizeCssAssetsPlugin({})
-        ],
-        splitChunks: {
-            chunks: 'all',
-            cacheGroups: {
-                libs: {
-                    name: "chunk-libs",
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: 10,
-                    chunks: "initial" // 只打包初始时依赖的第三方
-                }
-            }
+const config = WebpackMerge(webpackConfig, {
+  mode: 'production',
+  devtool: 'cheap-module-source-map',
+  optimization: {
+    minimizer: [
+      // new UglifyJsPlugin({//压缩js
+      //     cache: true,
+      //     parallel: true,
+      //     sourceMap: true
+      // }),
+      new WebpackParallelUglifyPlugin({
+        cacheDir: '.cache/',
+        uglifyJs: {
+          output: {
+            comments: false,
+            beatify: false
+          },
+          compress: {
+            drop_console: true,
+            collapse_Vars: true,
+            reduce_vars: true
+          }
         }
+      }),
+      new OptimizeCssAssetsPlugin({})
+    ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        libs: {
+          name: "chunk-libs",
+          test: /[\\/]node_modules[\\/]/,
+          priority: 10,
+          chunks: "initial" // 只打包初始时依赖的第三方
+        }
+      }
     }
+  },
+
 })
+
+if (process.env.npm_config_report) {
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+  config.plugins.push(new BundleAnalyzerPlugin({
+    openAnalyzer: true,
+    analyzerMode: 'static',
+    generateStatsFile: true
+  }))
+}
+
+module.exports = config
